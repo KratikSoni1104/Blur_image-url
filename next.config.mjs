86f59f71c271b7a13a5e7e2d28a518b583711afb
KratikSoni1104 <118619980+KratikSoni1104@users.noreply.github.com>
2024-03-01 11:05:41 +0530

    install older version

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-// import withPlaiceholder from '@plaiceholder/next';
+import withPlaiceholder from '@plaiceholder/next';
 const nextConfig = {
     images: {
         // domains: ['*'], // Use wildcard to allow all domains
@@ -12,4 +12,4 @@ const nextConfig = {
     }
 };
 
-export default nextConfig;
+export default withPlaiceholder(nextConfig);
